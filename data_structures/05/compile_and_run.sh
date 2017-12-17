0eda518f191aa0ba5cb44bab0fed9496b851c786
Łukasz Piłatowski <lpilatowski@teonite.com>
2017-12-17 15:21:38 +0100

    add more executions to compole and run script

--- data_structures/05/compile_and_run.sh
+++ data_structures/05/compile_and_run.sh
@@ -4,3 +4,13 @@ gcc 40016.sdizo.lab05.AVL.c -o list.out &&
     ./list.out 10000 0 &&
     ./list.out 15000 0 &&
     ./list.out 20000 0
+
+gcc 40016.sdizo.lab05.BST.c -o list.out &&
+    ./list.out 10000 0 &&
+    ./list.out 15000 0 &&
+    ./list.out 20000 0
+
+gcc 40016.sdizo.lab05.list.c -o list.out &&
+    ./list.out 10000 0 &&
+    ./list.out 15000 0 &&
+    ./list.out 20000 0
