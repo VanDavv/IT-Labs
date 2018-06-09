8ad4363b461cdea711e0acf3bf490f52c9502071
Łukasz Piłatowski <lpilatowski@teonite.com>
2018-06-10 00:26:41 +0200

    finish 03 and 04 for pradigmates

---
+++ languages_and_pradigmates/03/main.cpp
@@ -0,0 +1,373 @@
+#include <iostream>
+#include <cmath>
+#include <cassert>
+#include <numeric>
+#include <random>
+#include <chrono>
+#include <thread>
+#include <algorithm>
+#include <map>
+
+std::default_random_engine re;
+
+double rand_double(double min, double max) {
+    std::uniform_real_distribution<double> unif(min, max);
+    return unif(re);
+}
+
+
+int rand_int(int min, int max) {
+    std::uniform_int_distribution<int> unif(min, max);
+    return unif(re);
+
+}
+
+void print_progress(double progress) {
+    int barWidth = 70;
+
+    std::cout << "[";
+    auto pos = static_cast<int>(barWidth * progress);
+    for (int i = 0; i < barWidth; ++i) {
+        if (i < pos) std::cout << "=";
+        else if (i == pos) std::cout << ">";
+        else std::cout << " ";
+    }
+    std::cout << "] " << int(progress * 100.0) << " %\r";
+    std::cout.flush();
+}
+
+class Driver {
+public:
+    static int driver_count;
+    int getExperience() {
+        return experience;
+    }
+    int addExperience(int exp) {
+        switch (talent) {
+            case 1:
+                experience += std::log(exp + 1) * 1000;
+                break;
+            case 2:
+                experience += exp * 1.05;
+                break;
+            case 3:
+                experience += std::pow(exp, 2) / rand_int(1, 10);
+                break;
+            default:break;
+        }
+        return experience;
+    }
+    int getTalent() {
+        return talent;
+    }
+    Driver() {
+        talent = rand_int(1, 3);
+        driver_count++;
+    };
+    ~Driver() {
+        driver_count--;
+    }
+    static int getDriverCount() {
+        return driver_count;
+    }
+private:
+    int experience = 0;
+    int talent;
+};
+
+int Driver::driver_count = 0;
+
+class Car {
+public:
+    static int car_count;
+    int addDamage(int dmg) {
+        damage += dmg;
+        return damage;
+    }
+    int getValue() {
+        return value;
+    }
+    int getDamage() {
+        return damage;
+    }
+    static int getCarCount() {
+        return car_count;
+    }
+    Car() {
+        car_count++;
+    }
+    ~Car() {
+        car_count--;
+    }
+private:
+    int damage = 0;
+protected:
+    int value;
+};
+
+int Car::car_count = 0;
+
+class ExpensiveCar : public Car {
+public:
+    ExpensiveCar() : Car() {
+         value = 15000 * getCarCount();
+    }
+};
+
+class CommonCar : public Car {
+public:
+    CommonCar() : Car() {
+         value = 10000 * getCarCount();
+    }
+};
+
+class CheapCar : public Car {
+public:
+    CheapCar() : Car() {
+         value = 5000 * getCarCount();
+    }
+};
+
+class Game {
+public:
+    static Game* init() {
+        auto game = new Game();
+        game->drivers.push_back(new Driver());
+        game->cars.push_back(new CheapCar());
+        return game;
+    }
+    void buyDriver() {
+        int driver_price = 10000 * Driver::getDriverCount();
+
+        if (getBankBalance() < driver_price) {
+            std::cout << "Not enough funds! Need " << driver_price << std::endl;
+            return;
+        }
+
+        addBankBalance(-driver_price);
+        auto new_driver = new Driver();
+        drivers.push_back(new_driver);
+        switch(new_driver->getTalent()) {
+            case 1:
+                std::cout << "Purchased new driver with a little talent" << std::endl;
+                break;
+            case 2:
+                std::cout << "Purchased new driver with a moderate talent" << std::endl;
+                break;
+            case 3:
+                std::cout << "Purchased new driver with a great talent" << std::endl;
+                break;
+            default:
+                break;
+        }
+    }
+    void buyCar() {
+        int car_price, car_quality, base_car_price = 5000 * Car::getCarCount();
+
+        std::cout << "Pick car quality <1 - cheap(" << base_car_price << "), 2 - average("
+                  << base_car_price * 2 << "), 3 - expensive(" << base_car_price * 3 << ")>: ";
+        std::cin >> car_quality;
+        std::cout << std::endl;
+
+        if(car_quality < 1 || car_quality > 3) {
+            std::cout << "Aborted!" << std::endl;
+        }
+
+        car_price = base_car_price * car_quality;
+
+        if (getBankBalance() < car_price) {
+            std::cout << "Not enough funds! Need " << car_price << std::endl;
+            return;
+        }
+        addBankBalance(-car_price);
+        int index = Car::getCarCount();
+        switch(car_quality) {
+            case 1:
+                cars.push_back(new CheapCar());
+                break;
+            case 2:
+                cars.push_back(new CommonCar());
+                break;
+            case 3:
+                cars.push_back(new ExpensiveCar());
+                break;
+            default:
+                break;
+        }
+        std::cout << "You have purchased a new car!" << std::endl;
+    }
+
+    void sellCar() {
+        if(Car::getCarCount() <= 0) {
+            std::cout << "You don't own any cars!" << std::endl;
+            return;
+        }
+        char confirm;
+        std::cout << "Do you want to sell your latest car? (y - aggree): ";
+        std::cin >> confirm;
+        std::cout << std::endl;
+
+        if(confirm != 'y') {
+            std::cout << "Aborted!" << std::endl;
+            return;
+        }
+
+        Car *car_to_sell = cars[Car::getCarCount() - 1];
+
+        if(car_to_sell->getDamage() > 0) {
+            std::cout << "Your car is damaged, repair it before it can be sold!" << std::endl;
+            return;
+        }
+
+        auto car_income = static_cast<int>(car_to_sell->getValue() * 0.75);
+        addBankBalance(car_income);
+        delete car_to_sell;
+        std::cout << "You have sold your car for " << car_income << std::endl;
+    }
+    // Kontenery x2 rozne
+    void repairCars() {
+        int repaired = 0;
+        for (int i = 0; i < Car::getCarCount(); i++) {
+            Car* car = cars[i];
+            if(getBankBalance() >= car->getDamage()) {
+                addBankBalance(-car->getDamage());
+
+                if(car->getDamage() > 0) {
+                    repaired++;
+                }
+
+                car->addDamage(-car->getDamage());
+            } else {
+                std::cout << "Repaired " << repaired << " cars" << std::endl;
+                return;
+            }
+        }
+    }
+
+    void race() {
+        int min_count = std::min(Car::getCarCount(), Driver::getDriverCount());
+
+        typedef std::map<int, int> mymap;
+
+        mymap assigned_car;
+
+        int sum_exp = 0;
+
+        for (int i = 0; i < Driver::getDriverCount(); i++) {
+            sum_exp += drivers[i]->getExperience();
+        }
+
+        double collision_proba = 0.3 - (0.001 * sum_exp);
+
+
+        int choice;
+        for (int i = 0; i < min_count; i++) {
+            std::cout << "Pick car for driver #" << i + 1 << ":";
+            std::cin >> choice;
+
+            bool exists = false;
+            mymap::iterator it;
+            for (it = assigned_car.begin(); it != assigned_car.end(); ++it )
+                if (it->second == choice)
+                    exists = true;
+
+            if(exists || choice > Car::getCarCount() || choice <= 0) {
+                std::cout << "Invalid, try again!" << std::endl;
+                i--;
+                continue;
+            }
+
+            assigned_car[i] = choice;
+            bool is_colision = rand_double(0.0, 1.0) < collision_proba;
+            if (is_colision) {
+                drivers[i]->addExperience(700);
+                cars[assigned_car[i] - 1]->addDamage(static_cast<int>(cars[assigned_car[i] - 1]->getValue() * rand_double(0.001, 0.1)));
+                addBankBalance(rand_int(0, 1000));
+            } else {
+                drivers[i]->addExperience(2000);
+                addBankBalance(static_cast<int>(2000 + cars[assigned_car[i] - 1]->getValue() * 0.1 + rand_int(0, 1000)));
+            }
+        }
+
+        double progres = 0.0;
+        int wait_time = 10 * 1000;
+        while(progres <= 1.0) {
+            print_progress(progres);
+            std::this_thread::sleep_for(std::chrono::milliseconds(wait_time / 45));
+            progres += 1.0 / 45.0;
+        }
+    }
+
+    void printStatus() {
+        std::cout << std::endl << "Bank balance: " << getBankBalance() << std::endl;
+        std::cout << "Drivers: " << std::endl;
+        for(int i = 0; i < Driver::getDriverCount(); i++) {
+            std::cout << "#" << i + 1 << " (exp: " << drivers[i]->getExperience() << "; talent: " << drivers[i]->getTalent() << ") " << std::endl;
+        }
+        std::cout << "Cars: " << std::endl;
+        for(int i = 0; i < Car::getCarCount(); i++) {
+            std::cout << "#" << i + 1 << " (value: " << cars[i]->getValue() << "; damage: " << cars[i]->getDamage() << ") " << std::endl;
+        }
+    }
+private:
+    Game() {
+        bank_balance = 200000;
+    }
+    int getBankBalance() {
+        return bank_balance;
+    }
+    int addBankBalance(int balance) {
+        bank_balance += balance;
+        return bank_balance;
+    }
+    int bank_balance;
+    std::vector<Driver*> drivers;
+    std::vector<Car*> cars;
+};
+
+int main() {
+    Game* game = Game::init();
+
+    std::cout << "==============================" << std::endl
+              << "===== Raily Tycoon v.1.0 =====" << std::endl
+              << "==============================" << std::endl;
+
+    int choice;
+
+    do {
+        game->printStatus();
+        std::cout << "Options:" << std::endl
+                  << "  1. Race" << std::endl
+                  << "  2. Buy new driver" << std::endl
+                  << "  3. Buy new car" << std::endl
+                  << "  4. Sell a car" << std::endl
+                  << "  5. Repair cars" << std::endl
+                  << "  9. Exit" << std::endl << std::endl
+                  << "Your choice: ";
+        std::cin >> choice;
+
+        switch(choice) {
+            case 1:
+                game->race();
+                break;
+            case 2:
+                game->buyDriver();
+                break;
+            case 3:
+                game->buyCar();
+                break;
+            case 4:
+                game->sellCar();
+                break;
+            case 5:
+                game->repairCars();
+                break;
+            case 9:
+                break;
+            default:
+                std::cout << std::endl << "Incorrect choice, please try again!" << std::endl;
+                break;
+        }
+    } while(choice != 9);
+    return 0;
+}
---
+++ languages_and_pradigmates/04/script.js
@@ -0,0 +1,293 @@
+var readline = require('readline-sync');
+var sleep = require('sleep');
+
+const print_progress = (progress) => {
+    const barWidth = 70;
+
+    let message = "[";
+    const pos = Math.round(barWidth * progress);
+    for (let i = 0; i < barWidth; ++i) {
+        if (i < pos) {
+            message += "=";
+        }
+        else if (i == pos) {
+            message += ">";
+        }
+        else {
+            message += " "
+        }
+    }
+    message += "] " + Math.round(progress * 100) + "%";
+
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(message);
+};
+
+class Driver {
+    constructor() {
+        this.talent = Math.floor(Math.random() * 3);
+        this.experience = 0;
+        Driver.driver_count += 1;
+    }
+    getExperience() {
+        return this.experience;
+    }
+    addExperience(exp) {
+        switch (this.talent) {
+            case 1: this.experience += Math.log(exp + 1) * 1000; break;
+            case 2: this.experience += exp * 1.05; break;
+            case 3: this.experience += Math.pow(exp, 2) * Math.random() * 10; break;
+            default: break;
+        }
+        this.experience = Math.round(this.experience);
+        return this.experience;
+    };
+    getTalent() {
+        return this.talent;
+    }
+    static getDriverCount() {
+        return this.driver_count;
+    }
+}
+
+Driver.driver_count = 0;
+
+class Car {
+    constructor() {
+        Car.car_count += 1;
+        this.damage = 10;
+    }
+    addDamage(dmg) {
+        return (this.damage += Math.round(dmg));
+    }
+    getDamage() {
+        return this.damage;
+    }
+    getValue() {
+        return this.value;
+    }
+    static getCarCount() {
+        return this.car_count;
+    }
+}
+
+Car.car_count = 0;
+
+class ExpensiveCar extends Car {
+    constructor() {
+        super();
+        this.value = 15000 * Car.getCarCount();
+    }
+}
+
+class CommonCar extends Car {
+    constructor() {
+        super();
+        this.value = 10000 * Car.getCarCount();
+    }
+}
+
+class CheapCar extends Car {
+    constructor() {
+        super();
+        this.value = 5000 * Car.getCarCount();
+    }
+}
+
+class Game {
+    constructor() {
+        this.bank_balance = 200000;
+        this.drivers = [];
+        this.cars = [];
+    }
+    getBankBalance() {
+        return this.bank_balance;
+    }
+    addBankBalance(balance) {
+        return (this.bank_balance += Math.round(balance));
+    }
+    
+    static init() {
+        const game = new Game();
+        game.drivers.push(new Driver());
+        game.cars.push(new CheapCar());
+        return game;
+    }
+    buyDriver() {
+        const price = 10000 * Driver.getDriverCount();
+        if(this.getBankBalance() < price) {
+            console.error("Not enough funds! Need " + price);
+            return;
+        }
+        this.addBankBalance(-price);
+        const driver = new Driver();
+        this.drivers.push(driver);
+
+        switch(driver.getTalent()) {
+            case 1:
+                console.log("Purchased new driver with a little talent");
+                break;
+            case 2:
+                console.log("Purchased new driver with a moderate talent");
+                break;
+            case 3:
+                console.log("Purchased new driver with a great talent");
+                break;
+            default:
+                break;
+        }
+    };
+
+    buyCar() {
+        const base = 5000 * Car.getCarCount();
+        const quality = +(readline.question("Pick car quality <1 - cheap(" + base + "), 2 - average(" + base * 2 + "), 3 - expensive(" + base * 3 + ")>: "));
+        if(quality < 1 || quality > 3) {
+            console.error("Aborted!");
+            return
+        }
+        const price = quality * base;
+        if(this.getBankBalance() < price) {
+            console.error("Not enough funds! Need " + price);
+            return;
+        }
+        this.addBankBalance(-price);
+        this.cars.push(
+            quality === 1 ? new CheapCar() :
+            quality === 2 ? new CommonCar() :
+            new ExpensiveCar()
+        );
+        console.log("You have purchased a new car!");
+    };
+
+    sellCar() {
+        if(Car.getCarCount() <= 0) {
+            console.error("You don't own any cars!");
+            return;
+        }
+        const answer = readline.question("Do you want to sell your latest car? (y - aggree): ");
+        if(answer !== 'y') {
+            console.error("Aborted!");
+            return
+        }
+        const car = this.cars[this.cars.length - 1];
+        if(car.getDamage() > 0) {
+            console.error("Your car is damaged, repair it before it can be sold!");
+            return;
+        }
+        Car.car_count -= 1;
+        const removed = this.cars.pop();
+        this.addBankBalance(removed.getValue() * 0.75);
+        console.log("You have sold your car for " + removed.getValue() * 0.75)
+    };
+
+    repairCars() {
+        let repaired = 0;
+        for (const car of this.cars) {
+            if(this.getBankBalance() >= car.getDamage()) {
+                this.addBankBalance(-car.getDamage());
+
+                if(car.getDamage() > 0) {
+                    repaired++;
+                }
+
+                car.addDamage(-car.getDamage());
+            } else {
+                console.log("Repaired " + repaired + "cars");
+                return;
+            }
+        }
+    };
+
+    race() {
+        const min_count = Math.min(Car.getCarCount(), Driver.getDriverCount());
+        const exp = this.drivers.reduce((sum, item) => sum + item.getExperience(), 0);
+        const collision_proba = 0.3 - (0.001 * exp);
+
+        const assigned_cars = [];
+
+        for(let i = 0; i < min_count; i++) {
+            const choice = +(readline.question("Pick car for driver #" + (i + 1) + ":"));
+            if(assigned_cars.indexOf(choice) > -1 || choice > Car.getCarCount() || choice < 1) {
+                console.log("Invalid, try again!");
+                i--;
+                continue;
+            }
+            assigned_cars[i] = choice;
+            const assignes_car = this.cars[assigned_cars[i] - 1];
+            if(Math.random() < collision_proba) {
+                this.drivers[i].addExperience(700);
+                assignes_car.addDamage(assignes_car.getValue() * Math.random() / 10);
+                this.addBankBalance(Math.random() * 1000);
+            } else {
+                this.drivers[i].addExperience(2000);
+                this.addBankBalance(2000 + assignes_car.getValue() * 0.1 + Math.random() * 1000);
+            }
+        }
+
+        let progress = 0;
+        const wait_time = 10 * 1000;
+        while(progress < 1) {
+            print_progress(progress);
+            sleep.msleep(Math.floor(wait_time / 45));
+            progress += 1 / 45;
+        }
+    };
+    
+    printStatus() {
+        console.log();
+        console.log("Bank balance: " + this.getBankBalance());
+        console.log("Drivers: ");
+        for(let i = 0; i < Driver.getDriverCount(); i++) {
+            console.log("#" + (i + 1) + " (exp: " + this.drivers[i].getExperience() + "; talent: " + this.drivers[i].getTalent() + ")");
+        }
+        console.log("Cars:");
+        for(let i = 0; i < Car.getCarCount(); i++) {
+            console.log("#" + (i + 1) + " (value: " + this.cars[i].getValue() + "; damage: " + this.cars[i].getDamage() + ")");
+        }
+    }
+}
+
+const game = Game.init();
+console.log("==============================");
+console.log("===== Raily Tycoon v.1.0 =====");
+console.log("==============================");
+
+let choice = 0;
+
+while(choice !== 9) {
+    game.printStatus();
+    console.log("Options:" );
+    console.log("  1. Race");
+    console.log("  2. Buy new driver");
+    console.log("  3. Buy new car");
+    console.log("  4. Sell a car");
+    console.log("  5. Repair cars");
+    console.log("  9. Exit");
+    choice = +(readline.question("Your choice: "));
+
+    switch(choice) {
+        case 1:
+            game.race();
+            break;
+        case 2:
+            game.buyDriver();
+            break;
+        case 3:
+            game.buyCar();
+            break;
+        case 4:
+            game.sellCar();
+            break;
+        case 5:
+            game.repairCars();
+            break;
+        case 9:
+            break;
+        default:
+            console.log();
+            console.error("Incorrect choice, please try again!");
+            break;
+    }
+}
+        
