c4844bb249b8cf5dccb6c235439da875ce4cc7c4
Łukasz Piłatowski <lpilatowski@teonite.com>
2018-12-01 12:51:56 +0100

    add unity

---
+++ unity/scene/Scene/Assets/Scene/scripts/Rotable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotable : MonoBehaviour {
+	public float rotationSpeed = 1;
+	public bool rotateObject = false;
+	public Rotable centralObject;
+	
+	// Update is called once per frame
+	void Update () {
+		if (rotateObject)
+		{
+			if (centralObject.rotateObject)
+				transform.RotateAround(centralObject.centralObject.transform.position, Vector3.up, rotationSpeed * Time.deltaTime);
+			transform.RotateAround(centralObject.transform.position, Vector3.up, rotationSpeed * Time.deltaTime);
+		}
+	}
+}
