0d93cbe923e6e387087d3c10b1c17c987082478f
Łukasz Piłatowski <lukpila29@gmail.com>
2020-05-10 14:22:32 +0200

    finish lab11

---
+++ java_programming/lab11/Test.java
@@ -0,0 +1,11 @@
+package lab11;
+
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+
+public class Test {
+    public static void main(String... args) {
+        IntStream.range(1, 6).forEach(System.out::println);
+    }
+}
